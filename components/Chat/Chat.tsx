ab689037ac95b1299ab114d06bda84f5b9c85f63
StephenKarim <stephen.karim07@gmail.com>
2023-11-01 08:05:02 -0400

    names

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,17 +352,17 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to CurBot
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8">{`CurBot is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
+              Important: CurBot is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
+              CurBot allows you to plug in your API key to use this UI with
               their API.
             </div>
             <div className="mb-2">
@@ -404,11 +404,11 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                       <div>
                         <Spinner size="16px" className="mx-auto" />
                       </div>
-                    ) : (
+                    ) : (                      
                       'CurBot Chat'
                     )}
                   </div>
-{/* 
+{                 /* 
                   {models.length > 0 && (
                     <div className="flex h-full flex-col space-y-4 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
                       <ModelSelect />
