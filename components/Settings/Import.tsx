138131679719b346c4708a4b45dcc708abcc3c88
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-13 18:00:40 -0400

    Update Import.tsx

--- components/Settings/Import.tsx
+++ components/Settings/Import.tsx
@@ -1,20 +1,23 @@
-import { IconFileImport } from '@tabler/icons-react';
-import { FC } from 'react';
+import { IconFileImport } from '@tabler/icons-react'; // Importing the 'IconFileImport' component from the '@tabler/icons-react' library
+import { FC } from 'react'; // Importing the 'FC' type from the 'react' library
 
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from 'next-i18next'; // Importing the 'useTranslation' hook from 'next-i18next' for internationalization
 
-import { SupportedExportFormats } from '@/types/export';
+import { SupportedExportFormats } from '@/types/export'; // Importing the 'SupportedExportFormats' type from the 'export' module
 
-import { SidebarButton } from '../Sidebar/SidebarButton';
+import { SidebarButton } from '../Sidebar/SidebarButton'; // Importing the 'SidebarButton' component from the 'SidebarButton' module
 
+// Defining the props interface for the 'Import' component
 interface Props {
   onImport: (data: SupportedExportFormats) => void;
 }
-
+// Defining the 'Import' component as a functional component
 export const Import: FC<Props> = ({ onImport }) => {
-  const { t } = useTranslation('sidebar');
+  const { t } = useTranslation('sidebar'); / Using the 'useTranslation' hook to get the translation function 't' for the 'sidebar' namespace
+  // Returning the JSX elements for the 'Import' component
   return (
     <>
+      {/* Input element for file import, hidden from view */}
       <input
         id="import-file"
         className="sr-only"
@@ -22,22 +25,26 @@ export const Import: FC<Props> = ({ onImport }) => {
         type="file"
         accept=".json"
         onChange={(e) => {
+          // Check if files are selected
           if (!e.target.files?.length) return;
-
+          // Get the selected file
           const file = e.target.files[0];
           const reader = new FileReader();
+          // Read the content of the file
           reader.onload = (e) => {
             let json = JSON.parse(e.target?.result as string);
-            onImport(json);
+            onImport(json); // Invoke the 'onImport' callback with the parsed JSON data
           };
+          // Read the file as text
           reader.readAsText(file);
         }}
       />
-
+      {/* Button to trigger file import */}
       <SidebarButton
         text={t('Import data')}
         icon={<IconFileImport size={18} />}
         onClick={() => {
+          // Trigger the click event for the hidden file input element
           const importFile = document.querySelector(
             '#import-file',
           ) as HTMLInputElement;
