3485ed0c4e6d4e8824750e83a4021b811343e64c
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-13 19:07:13 -0400

    Update CodeBlock.tsx

--- components/Markdown/CodeBlock.tsx
+++ components/Markdown/CodeBlock.tsx
@@ -1,8 +1,10 @@
+// Importing necessary icons and libraries
 import { IconCheck, IconClipboard, IconDownload } from '@tabler/icons-react';
 import { FC, memo, useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
+// Importing translation utility and code generation functions
 import { useTranslation } from 'next-i18next';
 
 import {
@@ -10,60 +12,71 @@ import {
   programmingLanguages,
 } from '@/utils/app/codeblock';
 
+// Define Props interface for the CodeBlock component
 interface Props {
   language: string;
   value: string;
 }
-
+// CodeBlock component definition
 export const CodeBlock: FC<Props> = memo(({ language, value }) => {
-  const { t } = useTranslation('markdown');
-  const [isCopied, setIsCopied] = useState<Boolean>(false);
+  const { t } = useTranslation('markdown'); // Access translation function
+  const [isCopied, setIsCopied] = useState<Boolean>(false); // State for tracking copy success
 
+  // Function to copy code to clipboard
   const copyToClipboard = () => {
+    // Check if clipboard API is supported
     if (!navigator.clipboard || !navigator.clipboard.writeText) {
       return;
     }
 
+    // Write code to clipboard
     navigator.clipboard.writeText(value).then(() => {
-      setIsCopied(true);
+      setIsCopied(true); // Set copy success state and reset after 2 seconds
 
       setTimeout(() => {
         setIsCopied(false);
       }, 2000);
     });
   };
+  // Function to download code as a file
   const downloadAsFile = () => {
-    const fileExtension = programmingLanguages[language] || '.file';
-    const suggestedFileName = `file-${generateRandomString(
+    const fileExtension = programmingLanguages[language] || '.file'; // Determine file extension based on language or default to '.file'
+    const suggestedFileName = `file-${generateRandomString( 
       3,
       true,
-    )}${fileExtension}`;
+    )}${fileExtension}`;  // Generate a suggested file name
     const fileName = window.prompt(
       t('Enter file name') || '',
       suggestedFileName,
-    );
+    ); // Prompt user for file name
 
     if (!fileName) {
-      // user pressed cancel on prompt
+      // Check if user pressed cancel on prompt
       return;
     }
 
+    // Create a Blob containing the code
     const blob = new Blob([value], { type: 'text/plain' });
+    // Create a download link
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.download = fileName;
     link.href = url;
     link.style.display = 'none';
+    // Append the link to the document, click it, and remove it
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
     URL.revokeObjectURL(url);
   };
+  // Return the CodeBlock component UI
   return (
     <div className="codeblock relative font-sans text-[16px]">
       <div className="flex items-center justify-between py-1.5 px-4">
+        {/* Display the code language */}
         <span className="text-xs lowercase text-white">{language}</span>
 
+        {/* Copy to clipboard and download buttons */}
         <div className="flex items-center">
           <button
             className="flex gap-1.5 items-center rounded bg-none p-1 text-xs text-white"
@@ -72,6 +85,7 @@ export const CodeBlock: FC<Props> = memo(({ language, value }) => {
             {isCopied ? <IconCheck size={18} /> : <IconClipboard size={18} />}
             {isCopied ? t('Copied!') : t('Copy code')}
           </button>
+          {/* Download as file button */}
           <button
             className="flex items-center rounded bg-none p-1 text-xs text-white"
             onClick={downloadAsFile}
@@ -81,6 +95,7 @@ export const CodeBlock: FC<Props> = memo(({ language, value }) => {
         </div>
       </div>
 
+       {/* Syntax highlighting for the code */}
       <SyntaxHighlighter
         language={language}
         style={oneDark}
@@ -91,4 +106,4 @@ export const CodeBlock: FC<Props> = memo(({ language, value }) => {
     </div>
   );
 });
-CodeBlock.displayName = 'CodeBlock';
+CodeBlock.displayName = 'CodeBlock'; // Set display name for the CodeBlock component
