8700b9e862d86622124bbc1f47e6e4dfcc613a73
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-13 19:09:19 -0400

    Update MemoizedReactMarkdown.tsx

--- components/Markdown/MemoizedReactMarkdown.tsx
+++ components/Markdown/MemoizedReactMarkdown.tsx
@@ -1,8 +1,10 @@
-import { FC, memo } from 'react';
+import { FC, memo } from 'react'; // Importing React and necessary libraries
 import ReactMarkdown, { Options } from 'react-markdown';
 
+// Memoized version of ReactMarkdown component
 export const MemoizedReactMarkdown: FC<Options> = memo(
     ReactMarkdown,
+    // Memoization with custom equality check based on children prop
     (prevProps, nextProps) => (
         prevProps.children === nextProps.children
     )
