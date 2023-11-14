97ce05512a1170e3e66cce0fc3416b1897a9f6cf
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:14:39 -0400

    Update Conversations.tsx

--- components/Chatbar/components/Conversations.tsx
+++ components/Chatbar/components/Conversations.tsx
@@ -1,14 +1,19 @@
+// Importing necessary dependencies and components
 import { Conversation } from '@/types/chat';
 
 import { ConversationComponent } from './Conversation';
 
+// Props interface for Conversations component
 interface Props {
   conversations: Conversation[];
 }
 
+// Conversations functional component
 export const Conversations = ({ conversations }: Props) => {
+  // Rendering the component
   return (
     <div className="flex w-full flex-col gap-1">
+      {/* Mapping through conversations and rendering ConversationComponent */}
       {conversations
         .filter((conversation) => !conversation.folderId)
         .slice()
