3a7932e4d944296cda936ad492878860e28a3d04
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 16:32:57 -0400

    Update index.ts

--- pages/api/home/index.ts
+++ pages/api/home/index.ts
@@ -1,1 +1,1 @@
-export { default, getServerSideProps } from './home';
+export { default, getServerSideProps } from './home'; // Export the default export (Home component) and the getServerSideProps function from the './home' module
