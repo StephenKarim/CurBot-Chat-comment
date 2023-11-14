7aca109c81ed20b451d8d14e86e45e8c4a6d3f2e
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:36:10 -0400

    Update conversation.ts

--- utils/app/conversation.ts
+++ utils/app/conversation.ts
@@ -1,9 +1,11 @@
 import { Conversation } from '@/types/chat';
 
+// Update a conversation in the list of all conversations
 export const updateConversation = (
   updatedConversation: Conversation,
   allConversations: Conversation[],
 ) => {
+  // Map over all conversations and replace the old one with the updated version
   const updatedConversations = allConversations.map((c) => {
     if (c.id === updatedConversation.id) {
       return updatedConversation;
@@ -12,19 +14,23 @@ export const updateConversation = (
     return c;
   });
 
+  // Save the updated conversation and the updated list of all conversations to local storage
   saveConversation(updatedConversation);
   saveConversations(updatedConversations);
 
+  // Return an object with the updated single conversation and the updated list of all conversations
   return {
     single: updatedConversation,
     all: updatedConversations,
   };
 };
 
+// Save a single conversation to local storage
 export const saveConversation = (conversation: Conversation) => {
   localStorage.setItem('selectedConversation', JSON.stringify(conversation));
 };
 
+// Save a list of conversations to local storage
 export const saveConversations = (conversations: Conversation[]) => {
   localStorage.setItem('conversationHistory', JSON.stringify(conversations));
 };
