387caf2c12f9828b9eb7b47d519c0eb1b98435a5
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:45:34 -0400

    Update prompts.ts

--- utils/app/prompts.ts
+++ utils/app/prompts.ts
@@ -1,6 +1,8 @@
 import { Prompt } from '@/types/prompt';
 
+// Function to update a single prompt and save all prompts
 export const updatePrompt = (updatedPrompt: Prompt, allPrompts: Prompt[]) => {
+  // Update the specific prompt in the array of all prompts
   const updatedPrompts = allPrompts.map((c) => {
     if (c.id === updatedPrompt.id) {
       return updatedPrompt;
@@ -9,14 +11,17 @@ export const updatePrompt = (updatedPrompt: Prompt, allPrompts: Prompt[]) => {
     return c;
   });
 
-  savePrompts(updatedPrompts);
+  savePrompts(updatedPrompts); // Save the updated prompts to local storage
 
+  // Return the updated prompt and the array of all prompts
   return {
     single: updatedPrompt,
     all: updatedPrompts,
   };
 };
 
+// Function to save an array of prompts to local storage
 export const savePrompts = (prompts: Prompt[]) => {
+  // Save the array of prompts to local storage as a JSON string
   localStorage.setItem('prompts', JSON.stringify(prompts));
 };
