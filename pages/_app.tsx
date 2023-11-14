c66840ed337ee1645661a87ac692455ea4cb8fcb
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 16:52:03 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
+// Import necessary components and libraries
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+// Import i18n related utilities
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
+// Import global styles
 import '@/styles/globals.css';
 
+// Initialize the Inter font with Latin subset
 const inter = Inter({ subsets: ['latin'] });
 
+// Define the main App component
 function App({ Component, pageProps }: AppProps<{}>) {
+  // Create a new instance of QueryClient for React Query
   const queryClient = new QueryClient();
 
+  // Render the main application structure
   return (
     <div className={inter.className}>
       <Toaster />
@@ -22,4 +29,4 @@ function App({ Component, pageProps }: AppProps<{}>) {
   );
 }
 
-export default appWithTranslation(App);
+export default appWithTranslation(App); // Enhance the App component with translation capabilities
