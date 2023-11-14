cdfbc92c94ca95ca37f5cae5d10b7be60f027928
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:47:43 -0400

    Update settings.ts

--- utils/app/settings.ts
+++ utils/app/settings.ts
@@ -1,23 +1,30 @@
 import { Settings } from '@/types/settings';
 
+// Storage key for settings in local storage
 const STORAGE_KEY = 'settings';
 
+// Function to get user settings from local storage
 export const getSettings = (): Settings => {
-  let settings: Settings = {
+  let settings: Settings = { // Default settings with a dark theme
     theme: 'dark',
   };
+  // Retrieve saved settings from local storage
   const settingsJson = localStorage.getItem(STORAGE_KEY);
+   // If saved settings are found, try to parse and merge them with default settings
   if (settingsJson) {
     try {
       let savedSettings = JSON.parse(settingsJson) as Settings;
       settings = Object.assign(settings, savedSettings);
     } catch (e) {
-      console.error(e);
+      console.error(e); // Log an error if parsing fails
     }
   }
+  // Return the merged settings
   return settings;
 };
 
+// Function to save user settings to local storage
 export const saveSettings = (settings: Settings) => {
+  // Save the settings as a JSON string in local storage
   localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
 };
