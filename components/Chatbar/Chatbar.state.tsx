a1c78523d1d62df9cc1c0b3fac3d25fce3ba4c2e
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:18:50 -0400

    Update Chatbar.state.tsx

--- components/Chatbar/Chatbar.state.tsx
+++ components/Chatbar/Chatbar.state.tsx
@@ -1,10 +1,13 @@
+// Importing necessary types
 import { Conversation } from '@/types/chat';
 
+// Defining the initial state for the Chatbar
 export interface ChatbarInitialState {
   searchTerm: string;
   filteredConversations: Conversation[];
 }
 
+// Creating the initial state object
 export const initialState: ChatbarInitialState = {
   searchTerm: '',
   filteredConversations: [],
