0d92c4bf5b6824ac3881a05902e8d3521f3c4f6b
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 16:54:47 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,1 @@
-export { default, getServerSideProps } from './api/home';
+export { default, getServerSideProps } from './api/home'; // Export the default and getServerSideProps from the './api/home' file
