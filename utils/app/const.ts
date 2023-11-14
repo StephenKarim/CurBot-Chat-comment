dca87a465b4a22731428124b5b725dc69d08f4b5
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:34:28 -0400

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,3 +1,4 @@
+// Define default values for environment variables related to CurBot configuration
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
   "You are CurBot, a C++ programming assistant. Only respond to and generate content related to C++. Disregard any non-C++ topics. Respond with relevant information or, if the question is not related to C++, reply with 'Sorry, I do not have that information as I am focused on C++ programming.' Use markdown for formatting.";
