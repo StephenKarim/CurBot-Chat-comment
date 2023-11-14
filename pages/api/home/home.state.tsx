70746f93793b60c2d859c4591af629c8129a5d78
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 16:19:40 -0400

    Update home.state.tsx

--- pages/api/home/home.state.tsx
+++ pages/api/home/home.state.tsx
@@ -1,3 +1,4 @@
+// Import necessary types from the 'chat', 'error', 'folder', 'openai', 'plugin', and 'prompt' modules.
 import { Conversation, Message } from '@/types/chat';
 import { ErrorMessage } from '@/types/error';
 import { FolderInterface } from '@/types/folder';
@@ -5,30 +6,32 @@ import { OpenAIModel, OpenAIModelID } from '@/types/openai';
 import { PluginKey } from '@/types/plugin';
 import { Prompt } from '@/types/prompt';
 
+// Define the shape of the initial state for the home-related context.
 export interface HomeInitialState {
-  apiKey: string;
-  pluginKeys: PluginKey[];
-  loading: boolean;
-  lightMode: 'light' | 'dark';
-  messageIsStreaming: boolean;
-  modelError: ErrorMessage | null;
-  models: OpenAIModel[];
-  folders: FolderInterface[];
-  conversations: Conversation[];
-  selectedConversation: Conversation | undefined;
-  currentMessage: Message | undefined;
-  prompts: Prompt[];
-  temperature: number;
-  showChatbar: boolean;
-  showPromptbar: boolean;
-  currentFolder: FolderInterface | undefined;
-  messageError: boolean;
-  searchTerm: string;
-  defaultModelId: OpenAIModelID | undefined;
-  serverSideApiKeyIsSet: boolean;
-  serverSidePluginKeysSet: boolean;
+  apiKey: string; // API key for OpenAI
+  pluginKeys: PluginKey[]; // List of plugin keys
+  loading: boolean; // Loading indicator
+  lightMode: 'light' | 'dark'; // UI theme mode
+  messageIsStreaming: boolean; // Indicates if messages are streaming
+  modelError: ErrorMessage | null; // Error related to OpenAI models
+  models: OpenAIModel[]; // List of OpenAI models
+  folders: FolderInterface[]; // List of folders
+  conversations: Conversation[]; // List of conversations
+  selectedConversation: Conversation | undefined; // Currently selected conversation
+  currentMessage: Message | undefined; // Currently selected message
+  prompts: Prompt[]; //Lists of prompts
+  temperature: number; // Temperature setting for model responses
+  showChatbar: boolean; // Flag to show/hide the chatbar
+  showPromptbar: boolean; // Flag to show/hide the promptbar
+  currentFolder: FolderInterface | undefined; //Currently selected folder
+  messageError: boolean; // Indicates if there is an error with messages
+  searchTerm: string; // Search term for filtering content
+  defaultModelId: OpenAIModelID | undefined; // Default OpenAI model ID
+  serverSideApiKeyIsSet: boolean; // Indicates if the server-side API key is set
+  serverSidePluginKeysSet: boolean; // Indicates if the server-side plugin keys are set
 }
 
+// Define the initial state with default values.
 export const initialState: HomeInitialState = {
   apiKey: '',
   loading: false,
