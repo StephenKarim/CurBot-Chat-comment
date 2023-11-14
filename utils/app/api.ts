c5e88d2f1c83970bd39598a080b8ae451d4bbdd4
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:30:31 -0400

    Update api.ts

--- utils/app/api.ts
+++ utils/app/api.ts
@@ -1,13 +1,17 @@
+// Function to get the API endpoint based on the provided plugin
 import { Plugin, PluginID } from '@/types/plugin';
 
 export const getEndpoint = (plugin: Plugin | null) => {
+  // If no plugin is provided, use the default chat API endpoint
   if (!plugin) {
     return 'api/chat';
   }
 
+  // If the plugin is Google Search, use the Google API endpoint
   if (plugin.id === PluginID.GOOGLE_SEARCH) {
     return 'api/google';
   }
 
+  // Use the default chat API endpoint for other plugins
   return 'api/chat';
 };
