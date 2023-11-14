222d62bfe70305197386c6f996a65c5124af59d8
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:37:07 -0400

    Update folders.ts

--- utils/app/folders.ts
+++ utils/app/folders.ts
@@ -1,5 +1,6 @@
 import { FolderInterface } from '@/types/folder';
 
+// Save a list of folders to local storage
 export const saveFolders = (folders: FolderInterface[]) => {
   localStorage.setItem('folders', JSON.stringify(folders));
 };
