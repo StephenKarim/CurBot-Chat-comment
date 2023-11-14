3099a4344a769ff10bfba8718f696f23f83124e5
MariyaLewis <122990447+MariyaLewis@users.noreply.github.com>
2023-11-14 17:08:53 -0400

    Update ClearConversations.tsx

--- components/Chatbar/components/ClearConversations.tsx
+++ components/Chatbar/components/ClearConversations.tsx
@@ -1,3 +1,4 @@
+// Importing necessary dependencies and components
 import { IconCheck, IconTrash, IconX } from '@tabler/icons-react';
 import { FC, useState } from 'react';
 
@@ -5,21 +6,28 @@ import { useTranslation } from 'next-i18next';
 
 import { SidebarButton } from '@/components/Sidebar/SidebarButton';
 
+// Props interface for ClearConversations component
 interface Props {
   onClearConversations: () => void;
 }
 
+// ClearConversations functional component
 export const ClearConversations: FC<Props> = ({ onClearConversations }) => {
+  // State to manage the confirmation dialog
   const [isConfirming, setIsConfirming] = useState<boolean>(false);
 
+  // Translation hook
   const { t } = useTranslation('sidebar');
 
+  // Function to handle the clear conversations action
   const handleClearConversations = () => {
     onClearConversations();
     setIsConfirming(false);
   };
 
+  // Rendering the confirmation dialog or clear conversations button
   return isConfirming ? (
+    // Confirmation dialog
     <div className="flex w-full cursor-pointer items-center rounded-lg py-3 px-3 hover:bg-gray-500/10">
       
       
@@ -30,6 +38,7 @@ export const ClearConversations: FC<Props> = ({ onClearConversations }) => {
       </div>
 
       <div className="flex w-[40px]">
+        {/* Check icon to confirm the action */}
         <IconCheck
           className="ml-auto mr-1 min-w-[20px] text-neutral-400 hover:text-neutral-100"
           size={18}
@@ -39,6 +48,7 @@ export const ClearConversations: FC<Props> = ({ onClearConversations }) => {
           }}
         />
 
+        {/* Close icon to cancel the action */}
         <IconX
           className="ml-auto min-w-[20px] text-neutral-400 hover:text-neutral-100"
           size={18}
@@ -50,6 +60,7 @@ export const ClearConversations: FC<Props> = ({ onClearConversations }) => {
       </div>
     </div>
   ) : (
+    // Clear conversations button
     <SidebarButton
       text={t('Clear conversations')}
       icon={<IconTrash size={18} />}
