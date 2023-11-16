25870b22e42121d7e79b687a2dde3e8e58061dcf
issaboss123 <140730252+issaboss123@users.noreply.github.com>
2023-11-15 21:50:26 -0400

    comments

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,33 +1,33 @@
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'en', // Default locale when no language is explicitly specified
     locales: [
-      "bn",
-      "de",
-      "en",
-      "es",
-      "fr",
-      "he",
-      "id",
-      "it",
-      "ja",
-      "ko",
-      "pl",
-      "pt",
-      "ru",
-      "ro",      
-      "sv",
-      "te",
-      "vi",
-      "zh",
-      "ar",
-      "tr",
-      "ca",
-      "fi",
+      "bn", // Bengali
+      "de", // German
+      "en", // English
+      "es", // Spanish
+      "fr", // French
+      "he", // Hebrew
+      "id", // Indonesian
+      "it", // Italian
+      "ja", // Japanese
+      "ko", // Korean
+      "pl", // Polish
+      "pt", // Portuguese
+      "ru", // Russian
+      "ro", // Romanian
+      "sv", // Swedish
+      "te", // Telugu
+      "vi", // Vietnamese
+      "zh", // Chinese
+      "ar", // Arabic
+      "tr", // Turkish
+      "ca", // Catalan
+      "fi", // Finnish
     ],
   },
   localePath:
     typeof window === 'undefined'
-      ? require('path').resolve('./public/locales')
-      : '/public/locales',
+      ? require('path').resolve('./public/locales') // Path to locale files in the server-side
+      : '/public/locales', // Path to locale files in the client-side (browser)
 };
--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
+// Importing the i18n configuration from next-i18next.config.js
 const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
-  reactStrictMode: true,
+  i18n, // Using the i18n configuration
+  reactStrictMode: true, // Enabling React strict mode
 
+  // Customizing Webpack configuration
   webpack(config, { isServer, dev }) {
+    // Enabling experimental features in Webpack
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
     };
 
-    return config;
+    return config; // Returning the modified Webpack configuration
   },
 };
 
+// Exporting the Next.js configuration
 module.exports = nextConfig;
